24cce07c3b44f3ffbbb70388389fec37917deff3
Raizaz Achmad Asyraf <raizaz.achmad.asyraf@gmail.com>
2022-12-09 20:51:07 +0700

    Try CSRF fix

--- project_django/settings.py
+++ project_django/settings.py
@@ -145,4 +145,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-19b0.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-19b0.up.railway.app/account/register']
