c0f4dda72f543e9853eb085f7187658bb0d2d96d
David Christianto <christianto09@Alberts-MacBook-Pro.local>
2022-12-10 21:03:16 +0700

    review_add

--- review/views.py
+++ review/views.py
@@ -13,8 +13,11 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
+# utk nunjukin html nya
 def show_review(request):
     user_map = {}
     for us in get_user_model().objects.all():
@@ -22,6 +25,7 @@ def show_review(request):
     context = { "users": json.dumps(user_map) }
     return render(request, "review.html", context)
 
+# buat mskin review baru ke database
 def create_review(request):
     if request.method == 'POST':
         rating = request.POST.get('rating')
@@ -36,6 +40,22 @@ def create_review(request):
     
     return HttpResponseNotFound()
 
+# buat nge fetch data dari database ke bentuk json
 def show_json(request):
     data = Review.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def save_review(request):
+    if request.method == 'POST':
+        rating = request.POST.get('rating')
+        review_text = request.POST.get('review_text')
+        Review.objects.create(
+            user = request.user,
+            rating = rating,
+            date = datetime.datetime.now(),
+            review_text = review_text
+        )
+        return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
