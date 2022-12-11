e2237c901f8777e388e824a4db25edb53e562eef
David Christianto <christianto09@Alberts-MacBook-Pro.local>
2022-12-11 18:01:24 +0700

    rev2

--- review/urls.py
+++ review/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', show_review, name='show_review'),
     path('create/', create_review, name='create_review'),
     path('json/', show_json, name='show_json'),
-    path('profile_user/<str:user>/', profile_user, name='profile_user')
+    path('profile_user/<str:user>/', profile_user, name='profile_user'),
     path('save_review/', save_review, name='save_review')
 ]
